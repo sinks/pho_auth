10ac255c97744f906f9c6717b6de6723f62f98c7
Lincoln Fitzsimons <lincolnf@dius.com.au>
2017-03-19 21:01:50 +1100

    add warning to test for JWT HMAC encode
    the method Poison.encode may result in the json
    produced having the keys being in a different order then what they
    were passed in as.
    
    This would mean this test would fail but the code being correct.

--- test/services/jwt_test.exs
+++ test/services/jwt_test.exs
@@ -3,6 +3,9 @@ defmodule PhoAuth.JwtTest do
   alias PhoAuth.Jwt
 
   test "can encode HMAC SHA256 jwt token" do
+    # be warned
+    # that keys can change order when encoding to JSON
+    # which will change the result below
     jwt = Jwt.encode(:hmac, %{sub: "1234567890", name: "John Doe", admin: true}, "secret")
     assert jwt == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.pcHcZspUvuiqIPVB_i_qmcvCJv63KLUgIAKIlXI1gY8"
   end
