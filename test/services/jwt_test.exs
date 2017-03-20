ffa941f246bc64a7a22e6781ca29e2df7832f366
Lincoln Fitzsimons <lincolnf@dius.com.au>
2017-03-20 23:18:09 +1100

    fix failing jwt encode test

--- test/services/jwt_test.exs
+++ test/services/jwt_test.exs
@@ -6,7 +6,7 @@ defmodule PhoAuth.JwtTest do
     # be warned
     # that keys can change order when encoding to JSON
     # which will change the result below
-    jwt = Jwt.encode(:hmac, %{sub: "1234567890", name: "John Doe", admin: true}, "secret")
+    {:ok, jwt} = Jwt.encode("HS256", %{sub: "1234567890", name: "John Doe", admin: true}, "secret")
     assert jwt == "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.pcHcZspUvuiqIPVB_i_qmcvCJv63KLUgIAKIlXI1gY8"
   end
 end
